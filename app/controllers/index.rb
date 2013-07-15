1f4c9b25493034fd5697c88c3f4a90bd34121d93
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-15 15:45:17 -0500

    Update controller

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -8,13 +8,11 @@ post '/grandma' do
  user_input = params[:user_input]
  # If you typed in something in ALL CAPS make her respond humorously. 
  # If you typed in something else make her response with "Speak up, kiddo!"
- # if user_input == user_input.upcase
- 
- # elsif 
+ if user_input == user_input.upcase
+  result = "Something funny"
+ end 
   
  # end
-
- # debugger
-
+ 
  redirect "/?gradma=#{result}"
 end
